5f0159184a9613e21edff2c07740d7890b7c069c
Faisal Adnan <faisal.adnan@gmail.com>
2010-12-25 18:14:23 +0100

    Add method add/remove CanvasListener in Canvas class

--- src/com/frozenwired/Canvas.java
+++ src/com/frozenwired/Canvas.java
@@ -2,4 +2,6 @@ package com.frozenwired;
 
 public interface Canvas {
 	public void repaint();
+	public void addCanvasListener(CanvasListener canvasListener);
+	public void removeCanvasListener(CanvasListener canvasListener);
 }
