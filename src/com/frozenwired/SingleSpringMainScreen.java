98fed02ac683e4908f1c6bfefb77be2056ffc0fc
Faisal Adnan <faisal.adnan@gmail.com>
2010-12-25 14:23:44 +0100

    Refactoring SingleSpringMainScreen

--- src/com/frozenwired/SingleSpringMainScreen.java
+++ src/com/frozenwired/SingleSpringMainScreen.java
@@ -11,10 +11,11 @@ import net.rim.device.api.ui.component.TextField;
 import net.rim.device.api.ui.container.FlowFieldManager;
 import net.rim.device.api.ui.container.MainScreen;
 
-public class SingleSpringMainScreen extends MainScreen {
+public class SingleSpringMainScreen extends MainScreen implements AnimationListener {
 	TextField textField;
-	Spring spring = new Spring(2.5, 3, 0);
+	Spring spring = new Spring(2.5, 3, 0, 4);
 	SpringCanvas canvas = new SpringCanvas(spring);
+	SpringAnimation springAnimation = new SpringAnimation(spring, canvas);	
 	BasicEditField lenField = new BasicEditField("Len: ","2.500");	
 	public SingleSpringMainScreen()
 	{
@@ -23,7 +24,6 @@ public class SingleSpringMainScreen extends MainScreen {
 		add(new LabelField("Simulation Parameters:"));
 		add(textField);
 		add(new SeparatorField());
-		canvas.getSpring().move(2.5);
 		ButtonField lenButton = new ButtonField("Change");
 		FlowFieldManager lenManager = new FlowFieldManager();
 		lenManager.add(lenField);
@@ -31,27 +31,36 @@ public class SingleSpringMainScreen extends MainScreen {
 		add(canvas);
 		add(new SeparatorField());
 		add(lenManager);
-		
-		RungeKutta rk = new RungeKutta(this, canvas);
-		(new Thread(rk)).start();
+		springAnimation.addAnimationListener(this);
+		springAnimation.startAnimation();
 	}
-	public void updateText(String msg)
-	{
-		synchronized (UiApplication.getUiApplication().getEventLock()) {
-			textField.setText(msg);
-		}
-	}
+
 	protected void makeMenu(Menu menu, int instance)
 	{
 		super.makeMenu(menu, instance);
-		menu.add(new MenuItem("Change Length", 10, 10) {			
+		menu.add(new MenuItem("Restart Animation", 10, 10) {			
 			public void run() {
-				// TODO Auto-generated method stub
+				springAnimation.pauseAnimation();
 				String len = lenField.getText();				
-				canvas.getSpring().move(Double.parseDouble(len));
-				canvas.repaint();
+				spring.setInitialLen(Double.parseDouble(len));
+				springAnimation.startAnimation();
 			}
 		});
-		
+		menu.add(new MenuItem("Start Animation", 20, 10) {			
+			public void run() {
+				springAnimation.resumeAnimation();
+			}
+		});		
+		menu.add(new MenuItem("Pause Animation", 30, 10) {			
+			public void run() {
+				springAnimation.pauseAnimation();
+			}
+		});				
 	}
+
+	public void onAnimationChanged(String message) {
+		synchronized (UiApplication.getUiApplication().getEventLock()) {
+			textField.setText(message);
+		}				
+	}	
 }
