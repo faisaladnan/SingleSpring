03f2ec212d8932dd9e7dd977d1706d1f7a583e12
Faisal Adnan <faisal.adnan@gmail.com>
2010-12-26 15:58:40 +0100

    Add onCanvasReleased and onCanvasMoved methods

--- src/com/frozenwired/CanvasListener.java
+++ src/com/frozenwired/CanvasListener.java
@@ -2,5 +2,6 @@ package com.frozenwired;
 
 public interface CanvasListener {
 	public void onCanvasClicked(Canvas canvas);
-	public void onCanvasMoved(Canvas canvas);
+	public void onCanvasReleased(Canvas canvas);	
+	public void onCanvasMoved(Canvas canvas, int delta);
 }
