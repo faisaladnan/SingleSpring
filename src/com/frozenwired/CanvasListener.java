9fa72fb8643d1d7bc77bff381cadc1d7f94bc982
Faisal Adnan <faisal.adnan@gmail.com>
2010-12-28 01:50:21 +0100

    Add TouchScreen support

--- src/com/frozenwired/CanvasListener.java
+++ src/com/frozenwired/CanvasListener.java
@@ -4,4 +4,6 @@ public interface CanvasListener {
 	public void onCanvasClicked(Canvas canvas);
 	public void onCanvasReleased(Canvas canvas);	
 	public void onCanvasMoved(Canvas canvas, int delta);
+	public void onCanvasTouchDown(Canvas canvas);
+	public void onCanvasTouchUp(Canvas canvas);
 }
